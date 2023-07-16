bc8b37deeac9be4683b7c55f6c58e29dc12c05b7
TaifoorFarid <136922765+TaifoorFarid@users.noreply.github.com>
2023-07-16 04:07:25 -0400

    Update 2d__Array_Sort_Pointers.cpp

--- Arrays/2d__array_sort_pointers.cpp
+++ Arrays/2d__array_sort_pointers.cpp
@@ -4,39 +4,33 @@
 void print(int a[][5], int size)
 {
 	for(int i = 0 ; i < size ; i++)
-	{
+	{	
 		for(int j = 0 ; j < size ; j++)
-    	printf("%d ", a[i][j]);
-    	printf("\n");
+                	printf("%d ", a[i][j]);
+                	printf("\n");
 	}
 }
 
 void sort(int a[][5], int size)
 {
-	int count = 0;
 	for(int i = 1 ; i < size*size ; i++)
 	{
-    	int *start = a[0];
-    	int flag = 1;
+         	int *start = a[0];
+        	int flag = 1;
 		for(int j = 0 ; j < size*size - i ; j++)
-    	{
-	    	if(*start > *(start+1))
-	        	{
-	    		flag = 0;
-		    	int temp = *start;
-		    	*start = *(start + 1);
-		    	*(start + 1) = temp;
+         	{
+	         	if(*start > *(start+1))
+                	{
+	         		flag = 0;
+	        	    	int temp = *start;
+	        	    	*start = *(start + 1);
+	        	    	*(start + 1) = temp;
 	        	}
-	    start++;
-	    count++;
-       	} 
-       	if(flag)
-       	{
-		    printf("\nItreations saved = %d\n", 300 - count);
-	        return;
-	         
-	    }
-	}
+        	    start++;
+         	} //End of inner for
+         	if(flag)
+	            return; 
+	}//End of outer for
 }
 
 main()
